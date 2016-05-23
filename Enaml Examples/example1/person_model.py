cd2415908527b52d5617968a8ee48b65df69ce8b
jackaixin <jackaixin@192.168.0.8>
2016-05-23 19:54:46 -0400

    Changed person_view so that full name now subscribes to changes in first name and last name

--- Enaml Examples/example1/person_model.py
+++ Enaml Examples/example1/person_model.py
@@ -4,8 +4,8 @@ person_model
 @author: jackaixin
 '''
 
-from atom.api import Atom, Unicode
+from atom.api import Atom, Str
 
 class Person(Atom):
-    first_name = Unicode()
-    last_name = Unicode()
+    first_name = Str()
+    last_name = Str()
